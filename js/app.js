27f25f2b49a0a6c0143b8a052c491943affea584
Matt Katz <mrmattkatz@gmail.com>
2015-11-12 14:28:30 -0600

    Search function add attempt

--- js/app.js
+++ js/app.js
@@ -1,11 +1,32 @@
-$.ajax({
-    type: "GET",
-    dataType: "jsonp",
-	cache: true,
-    url: "https://api.instagram.com/v1/tags/consumeraffairs/media/recent?access_token=1137819202.4400571.ddb143985bbe4037a23664722dcd79a4",
-    success: function(data) {
-        for (var i = 0; i < 20; i++) {
-            $("#instagram").append("<div><a href='" + data.data[i].link + "'><img src='" + data.data[i].images.low_resolution.url +"'></img></a></div>");
-        }
-    }
+$(document).ready(function() {
+    //API called when search button clicked
+    $('#search').on('click', function(e) {
+        //clear ig-div
+        var imgDiv = $('.ig-div');
+        imgDiv.remove();
+        //clear HTML for each new load
+        $('#instagram').html("");
+        //search value entered
+        var searchValue = $('#search-input').val();
+        //API call
+        $.ajax({
+            type: "GET",
+            dataType: "jsonp",
+        	cache: true,
+            url: "https://api.instagram.com/v1/tags/consumeraffairs/media/recent?access_token=1137819202.4400571.ddb143985bbe4037a23664722dcd79a4",
+            success: function(data) {
+                //loops threw to load 20 images
+                for (var i = 0; i < 20; i++) {
+                    var usertag = data.data[i].user.id;
+                    //search via usertag
+                    if (usertag === searchValue) {
+                        $("#instagram").append("<div><a href='" + data.data[i].link + "'><img src='" + data.data[i].images.low_resolution.url +"'></img></a></div>");
+                    //if no usertag entered, load all images
+                    } else {
+                        $("#instagram").append("<div><a href='" + data.data[i].link + "'><img src='" + data.data[i].images.low_resolution.url +"'></img></a></div>");
+                    }
+                }
+            }
+        });
+    });
 });
